93d31c3c91438ba8e83cc9f94eaafd98a3b07fa0
Enrico <32924566+enricopau@users.noreply.github.com>
2023-02-28 21:04:55 +0100

    wip removeunconfirmedticker

--- database/mongodb.go
+++ database/mongodb.go
@@ -28,7 +28,18 @@ type MailEntry struct {
 	Mail         string             `bson:"mail",omitempty`
 	Token        string             `bson:"token",omitempty`
 	Confirmed    bool               `bson:"confirmed",omitempty`
-	TimeCreation time.Time          `bson:"timestamp",omitempty`
+	CreationTime primitive.DateTime `bson:"timestamp",omitempty`
+}
+
+// NewMailEntry creates a new MailEntry with the a new object ID and sets the CreationTime to time.Now().
+func NewMailEntry(mail, token string, confirmed bool) *MailEntry {
+	return &MailEntry{
+		ID:           primitive.NewObjectID(),
+		Mail:         mail,
+		Token:        token,
+		Confirmed:    confirmed,
+		CreationTime: primitive.NewDateTimeFromTime(time.Now()),
+	}
 }
 
 func (e *MailEntry) Marshal() ([]byte, error) {
@@ -45,17 +56,17 @@ const (
 )
 
 // Connect reads the URI string from the given .env file or other environment variables and connects to the mongodb client.
-func Connect() (*mongo.Client, error, func(ctx context.Context) error) {
+func Connect() (*mongo.Client, func(ctx context.Context) error, error) {
 	err := godotenv.Load()
 	uri := os.Getenv("MONGODB_URI")
 	if uri == "" {
-		return nil, fmt.Errorf("no connection uri set"), nil
+		return nil, nil, fmt.Errorf("no connection uri set")
 	}
 	if err != nil {
-		return nil, err, nil
+		return nil, nil, err
 	}
 	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
-	return client, err, client.Disconnect
+	return client, client.Disconnect, err
 }
 
 // MailList is a wrapper for querying the maillist collection
@@ -67,14 +78,14 @@ func MailListCollection(client *mongo.Client) *mongo.Collection {
 func CreateTestDataset(n int) (entries []*MailEntry) {
 	r := rand.Intn(99999)
 	for i := 0; i < n; i++ {
-		entry := *&MailEntry{
+		entry := &MailEntry{
 			ID:           primitive.NewObjectID(),
 			Mail:         fmt.Sprintf("%d@test.test", r),
 			Token:        "0000",
 			Confirmed:    false,
-			TimeCreation: time.Now(),
+			CreationTime: primitive.NewDateTimeFromTime(time.Now()),
 		}
-		entries = append(entries, &entry)
+		entries = append(entries, entry)
 	}
 	return entries
 }
--- main.go
+++ main.go
@@ -4,39 +4,49 @@ import (
 	"context"
 	"fmt"
 	"log"
-	"net/http"
 	"time"
 
 	"github.com/enricopau/gosub/database"
-	"github.com/enricopau/gosub/server"
 )
 
 func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Minute)
 	defer cancel()
-	client, err, disconnect := database.Connect()
-	defer disconnect(ctx)
+
+	client, dc, err := database.Connect()
+	defer dc(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	twelve := time.Now().Add(-12 * time.Hour)
+	fmt.Printf("Time: %s\n", twelve)
+	err = database.DeleteEntriesAfter(ctx, twelve, database.MailListCollection(client))
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	mail, err := database.Mail(ctx, "67267@test.test", database.MailListCollection(client))
+	// g := server.NewGlue(ctx, "127.0.0.1:8000")
+	// g.StartServer()
+}
+
+func RemoveCheck() {
+	// make this like 10 hours in PROD
+	t := time.NewTicker(12 * time.Second)
+
+	for {
+		select {
+		case <-t.C:
+			removeAfter(ctx)
+		}
+	}
+}
+
+func removeAfter(ctx context.Context) error {
+	client, dc, err := database.Connect()
+	defer dc(ctx)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
-	fmt.Println(mail)
-
-	glue, err := server.New()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	srv := &http.Server{
-		Handler:      glue.Router,
-		Addr:         "127.0.0.1:8000",
-		WriteTimeout: 15 * time.Second,
-		ReadTimeout:  15 * time.Second,
-	}
-	log.Fatal(srv.ListenAndServe())
-
+	err = database.DeleteEntriesAfter(ctx, time.Now().Add(-12*time.Hour), database.MailListCollection(client))
+	return err
 }
